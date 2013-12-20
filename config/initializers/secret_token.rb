2bddc5809f77f1d977a10df26d208d36e51ca908
Thomas Bergman <thomas.e.bergman@gmail.com>
2013-12-20 01:16:51 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carpool::Application.config.secret_key_base = '975801d469141f421a263f2fcedca786869e8b90c5336ce1d821a12c5f67e1dc0d6e0721bd81010ec8951043c246fcd879ff8a2aac884ad9cc8a04ecc9a88d0c'
