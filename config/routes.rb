b4eaf254d285994153088031e1cf615f27d314a9
Thomas Bergman <thomas.e.bergman@gmail.com>
2013-12-23 12:31:44 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Carpool::Application.routes.draw do
   get "static_pages/home", as: "home"
-  get "static_pages/help"
+  get "static_pages/help", as: "help"
+  get "static_pages/about", as: "about"
   get "log_in" => "sessions#new", as: "log_in"
   get "log_out" => "sessions#destroy", as: "log_out"
   get "sign_up" => "users#new", as: "sign_up"
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Carpool'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Carpool')
+    end
+
+    it "should have the right title" do
+  		visit '/static_pages/home'
+  		expect(page).to have_title("Carpool | Home")
+		end
+  end
+
+  describe "Help page" do
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_content('Help')
+  	end
+
+  	it "should have the right title" do
+  		visit '/static_pages/help'
+  		expect(page).to have_title("Carpool | Help")
+		end
+  end
+
+  describe "About page" do
+  	it "should have the content 'About'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About')
+  	end
+
+  	it "should have the right title" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title("Carpool | About")
+		end
+  end
+end
